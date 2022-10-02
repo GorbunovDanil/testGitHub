ce55588dd84edca172720f51d1064e6c99bee587
Danil Gorbunov <91661316+GorbunovDanil@users.noreply.github.com>
2022-10-02 16:29:51 +0200

    Add files via upload

---
+++ testGitHub/testProject/src/main/java/com/mycompany/testproject/TestProject.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SoucetSoucin {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int firstNum = sc.nextInt();
+
+        int secondNum = sc.nextInt();
+
+        System.out.println(firstNum + secondNum);
+
+        System.out.println(firstNum * secondNum);
+        
+    }
+}
